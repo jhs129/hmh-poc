40eb355ce4f60b9a25227cfe99758f9f663ad8c4
jhs129 <john.h.schneider@gmail.com>
2023-11-24 06:42:11 -0500

    fixed right side gap on mobile

--- components/layout/header.jsx
+++ components/layout/header.jsx
@@ -29,52 +29,63 @@ function Header(props) {
   };
 
   return (
-    <header className="site-container p-8 md:pt-4">
+    <header className="site-container pt-8 md:py-4">
       <div className="flex gap-4 max-md:flex-wrap">
-        <div className="flex flex-row space-y-6 space-x-40">
+        <div id="logo" className="flex flex-row w-full md:w-80 justify-between">
           {/* Logo Image */}
-          <Image src={logoImage} alt="Logo" width={200} height={200} />
+          <div className="flex">
+            <Image src={logoImage} alt="Logo" width={200} height={200} />
+          </div>
+
           {/*  Hamburger Button */}
-          <button
-            id="menu-btn"
-            className={`block md:mr-16 md:hidden hamburger focus:outline-none ${
-              isOpen ? "open" : ""
-            }`}
-            type="button"
-            onClick={() => {
-              toggleOpen();
-              toggleMenu();
-            }}
-          >
-            <span class="hamburger-top"></span>
-            <span class="hamburger-middle"></span>
-            <span class="hamburger-bottom"></span>
-          </button>
+          <div className="flex items-center justify-end">
+            <button
+              id="menu-btn"
+              className={`block md:hidden hamburger focus:outline-none ${
+                isOpen ? "open" : ""
+              }`}
+              type="button"
+              onClick={() => {
+                toggleOpen();
+                toggleMenu();
+              }}
+            >
+              <span class="hamburger-top"></span>
+              <span class="hamburger-middle"></span>
+              <span class="hamburger-bottom"></span>
+            </button>
+          </div>
         </div>
+
         {/* Desktop Nav */}
-        <div className="flex flex-col self-start md:max-w-full md:items-end">
+        <div id="nav" className="flex flex-col w-full self-start md:max-w-full md:items-end">
           {/* Search Form */}
-          <div className="px-4 mx-auto md:m-0">
-            <form className="border flex w-[400px] max-w-full items-stretch gap-0 p-px border-solid border-orange-600 text-white">
-              <input
-                type="text"
-                className="bg-white flex w-[338px] shrink-0 h-[35px] flex-col placeholder-orange-600 p-2"
-                aria-label="Search"
-                placeholder="Search Our Network"
-              />
-              <input
-                type="button"
-                className="bg-orange-600 w-full text-center"
-                value="Go"
-              />
+
+          <div className="mx-auto w-full justify-end md:w-80 md:m-0">
+            <form className="flex flex-row border w-full justify-items-stretch border-solid border-orange-600 text-white">
+              
+                <input
+                  type="text"
+                  className="bg-white w-5/6 flex flex-col placeholder-orange-600 p-2"
+                  aria-label="Search"
+                  placeholder="Search Our Network"
+                />
+              
+                <input
+                  type="button"
+                  className="bg-orange-600 w-1/6 justify-center"
+                  value="Go"
+                />
             </form>
           </div>
+
           {/* Navigation */}
           <nav>
             {/* Desktop Navigation */}
+
             <div
               id="desktop-nav"
-              className="hidden md:flex self-stretch items-end justify-between gap-5 mt-3 px-5 max-md:max-w-full max-md:flex-wrap max-md:justify-center "
+              className="hidden md:flex self-stretch items-end justify-between gap-5 mt-3 max-md:max-w-full max-md:flex-wrap max-md:justify-center "
             >
               {navigation.navigation.map((item, index) => (
                 <Link
@@ -86,7 +97,9 @@ function Header(props) {
                 </Link>
               ))}
             </div>
+
             {/* Mobile Navigation */}
+
             <div
               id="mobile-nav"
               className="hidden flex-col mx-auto items-start p-4 text-center md:hidden"
